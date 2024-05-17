4ec90d9a8cb21b5886c42c52038409aec68f8625
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-05-17 21:40:05 +0100

    Add imports and scope to access googlesheet

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import time
+import threading
+
+## access to googlesheet
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open('hangman-words').sheet1
