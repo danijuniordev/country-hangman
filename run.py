d74613eeb1e174995f8b033293b916c3becbe227
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-05-17 21:50:41 +0100

    Add game start draw

--- run.py
+++ run.py
@@ -12,9 +12,21 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open('hangman-words').sheet1
 
+game_start = '''
+    █     █   ████████   ██      █  ███████    ██       ██   ████████   ██      █ 
+    █     █   █      █   █ █     █  █          █ █     █ █   █      █   █ █     █
+    █     █   █      █   █  █    █  █          █  █   █  █   █      █   █  █    █
+    ███████   ████████   █   █   █  █   ████   █   █ █   █   ████████   █   █   █
+    █     █   █      █   █    █  █  █      █   █    █    █   █      █   █    █  █
+    █     █   █      █   █     █ █  █      █   █         █   █      █   █     █ █ 
+    █     █   █      █   █      ██  ████████   █         █   █      █   █      ██
+'''
+
+
 def clear_terminal():
     '''
     Clears the terminal.
     '''
 
     os.system('cls' if os.name == 'nt' else 'clear')
+
